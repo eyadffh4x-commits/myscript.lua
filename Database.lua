1385173596a5072d6d5f33f8edc0fa91e0e8d9bc
eyadffh4x-commits <eyadffh4x@gmail.com>
2026-01-03 06:55:10 +0200

    Add files via upload

---
+++ Database.lua
@@ -0,0 +1,44 @@
+
+
+return {
+    
+    
+    ["4DAYS"] = {
+        is_expired = false,  
+        msg = "VIP 👑"
+    },    
+    
+    
+    
+     ["JJ_A9"] = {
+        is_expired = false,  
+        msg = "VIP 👑"
+    },   
+    
+    
+        ["admin"] = {
+        is_expired = false,  
+        msg = "VIP 👑"
+    },
+    
+    
+    
+    
+    ["VI_P"] = {
+        is_expired = false,  
+        msg = "VIP 👑"
+    },
+
+
+    ["USER_123"] = {
+        is_expired = true,   
+        
+        msg = "عفواً، اشتراكك انتهى ❌"
+    },
+
+   
+    ["TEST"] = {
+        is_expired = false,
+        msg = "3Day✅"
+    }
+}
